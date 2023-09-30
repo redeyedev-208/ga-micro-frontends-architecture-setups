c51a5d7cc43879b9fb5b74994fb7d8381f174bac
gilaguilar <redeyedev.208@gmail.com>
2023-09-30 00:32:09 -0600

    addressing scenarios for isolated usage and team usage of html element consideration being consumed and rendered

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,14 +1,46 @@
 // We want to generate a list of products and display them on the page
 import faker from 'faker';
 
-let products = '';
+// We are refactoring our code to be reusable and have a more verbose implementation
+// This file will be used in both development and production
+// We are being proactive and making sure that we are not immediately rendering our app
+// Due to how teams will be using this file, we are not going to assume that an element with an id of dev-products exists
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+const mount = (el) => {
+  let products = '';
+
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  // To accomplish this in React we would use ReactDOM.render(<App />, el)
+  el.innerHTML = products;
+};
+
+// Context/Situation #1 - We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of dev-products
+// We want to immediately render our app into that element
+// The env variable is set in webpack.config.js
+
+// Assuming our container doesn't have an element with an id of dev-products
+// We are running this file in isolation
+// We are using our local index.html file
+// Which DEFINITELY has an element with an id of dev-products
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-faker-products');
+
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-// We are going to use webpack to bundle our code
-// We are going to use webpack-dev-server to serve up our code
-// 1st so that we output onto the page for the products endpoint -> console.log(products);
-document.querySelector('#dev-faker-products').innerHTML = products;
+// Context/Situation #2 - We are running this file in development or production
+// We are running this file in the CONTAINER app
+// There is no guarantee that an element with an id of dev-products exists
+// We DO NOT want to immediately render our app into that element
+// Our container will call mount (which will be defined in the container)
+export { mount };
